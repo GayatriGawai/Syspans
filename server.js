09e25a97a8c810343bdd07de37d29be981bfb6aa
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2023-12-13 19:50:17 +0530

    Established the connection between mongoose and mongoDB

--- server.js
+++ server.js
@@ -3,3 +3,19 @@ const app = express();
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+//Connection
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log('Connection established');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+connectDB();
