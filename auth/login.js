141261890f8847e9c278f870bf1e34c0bc25b1a5
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2023-12-18 18:46:34 +0530

    Created the front end of the admin_dashboard

--- auth/login.js
+++ auth/login.js
@@ -3,9 +3,14 @@ function validateLogin() {
     var password = document.getElementById('password').value;
 
     if (email === 'admin@gmail.com' && password === 'admin') {
-        alert('Login successful!');
-        window.location.href = 'dashboard.html';
+        alert('Logged in as admin!');
+        window.location.href = 'admin_dashboard.html';
+    } else if (email === 'emp@gmail.com' && password === '12345678') {
+        alert('Logged in as employee');
+        window.location.href = 'emp_dashboard.html';
     } else {
         alert('Invalid username or password. Please try again.');
     }
+
+    return false;
 }
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
+// WE HAD ADD THE PATHS IN NEXT Session
+
 //Connection
 const mongoose = require('mongoose');
 const config = require('config');
